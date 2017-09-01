52dd7b05d3202d1cde2a6acabc0d942e01d2d52f
Lucas Blancas <lablancas@gmail.com>
2017-09-01 14:50:16 -0500

    use google places to find a place matching the users text then use that location to find nearby shelters

--- receive/receive-message.js
+++ receive/receive-message.js
@@ -1,7 +1,14 @@
+import _ from 'lodash';
 import { sendTextMessage } from '../send/send-text-message';
 import { sendSheltersMessage } from '../send/send-shelters';
 import { sendLocationRequest } from '../send/send-location-request';
 import { sendQuickReply } from '../send/send-quick-reply';
+
+import maps from '@google/maps';
+
+const client = maps.createClient({
+  key: process.env.GOOGLE_PLACES_API_KEY
+});
 
 export const receivedMessage = event => {
   const senderID = event.sender.id;
@@ -29,6 +36,11 @@ export const receivedMessage = event => {
 
       case 'Help':
         sendQuickReply(senderID, 'How can I help?');
+      default:
+        client.places({ query: messageText }, (err, response) => {
+            const place = _.first(response.json.results);
+            place && sendSheltersMessage(senderID, place.geometry.location);
+        });
     }
   } else if (messageAttachments && messageAttachments[0].type === 'location') {
     sendSheltersMessage(senderID, messageAttachments[0].payload.coordinates);
