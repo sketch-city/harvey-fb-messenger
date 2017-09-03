ae12220323c55a32c0923051dc2b6bc539dbfed0
Lucas Blancas <lablancas@gmail.com>
2017-09-03 09:41:36 -0500

    add call to action button to volunteer info cards

--- send/send-volunteer-info.js
+++ send/send-volunteer-info.js
@@ -16,11 +16,21 @@ export const sendVolunteerInfo = recipientId => {
                     "title": "Volunteer Map",
                     "subtitle": "Find verified places to volunteer",
                     "item_url": "https://sketch-city.github.io/harvey-needs/",
+                    buttons: [{
+                      type: "web_url",
+                      url: "https://sketch-city.github.io/harvey-needs/",
+                      title: "View Map"
+                    }]
                   },
                   {
                     "title": "Muck Map",
                     "subtitle": "Find places that need to be mucked",
                     "item_url": "https://www.texasrescuemap.com/muckmap",
+                    buttons: [{
+                      type: "web_url",
+                      url: "https://www.texasrescuemap.com/muckmap",
+                      title: "View Map"
+                    }]
                   },
               ],
             }
